1b0d6cad1468abaa2f558b912f241245b6210fcd
iiaii <jhun9409@naver.com>
2021-04-21 23:42:39 +0900

    project init

---
+++ src/test/java/me/iiaii/springdatajpaintro/SpringDataJpaIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package me.iiaii.springdatajpaintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
